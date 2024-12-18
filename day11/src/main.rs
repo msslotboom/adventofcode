d5dc4490fc806c029f01858c1cdb7a88e0ecb4e6
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2024-12-18 11:03:35 +0200

    Day 11 part 2

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,24 +1,34 @@
+use std::collections::HashMap;
+
+
+
 fn main() {
-    let mut input: Vec<u64> = vec![125, 17];
-    for _ in 0..25{
-        input = blink(input);
+    let input: Vec<u64> = vec![125, 17];
+    let mut stones: HashMap<u64, u64> = HashMap::new();
+    for stone in &input{
+        stones.insert(*stone, 1);
     }
-    println!("{:?}", input.len())
+    for _ in 0..75{
+        stones = blink(&stones);
+    }
+    println!("{:?}", stones.values().sum::<u64>())
 }
 
-fn blink(stones: Vec<u64>) -> Vec<u64> {
-    let mut new_stones = vec![];
-    for stone in stones{
-        if stone == 0 {
-            new_stones.push(1)
+fn blink(stones: &HashMap<u64, u64>) -> HashMap<u64, u64> {
+    let mut new_stones: HashMap<u64, u64> = HashMap::new();
+    for stone in stones.keys(){
+        if *stone == 0 {
+            new_stones.entry(1).and_modify(|v| *v += stones[stone]).or_insert(stones[stone]);
         }
         else if stone.to_string().len() % 2 == 0 {
             let stonestr = stone.to_string();
-            new_stones.push(stonestr[..stonestr.len()/2].parse().unwrap());
-            new_stones.push(stonestr[stonestr.len()/2..].parse().unwrap());
+            let val1 = stonestr[..stonestr.len()/2].parse().unwrap();
+            let val2 = stonestr[stonestr.len()/2..].parse().unwrap();
+            new_stones.entry(val1).and_modify(|v| *v += stones[stone]).or_insert(stones[stone]);
+            new_stones.entry(val2).and_modify(|v| *v += stones[stone]).or_insert(stones[stone]);
         }
         else {
-            new_stones.push(stone * 2024)
+            new_stones.entry(stone * 2024).and_modify(|v| *v += stones[stone]).or_insert(stones[stone]);
         }
     }
     return new_stones
